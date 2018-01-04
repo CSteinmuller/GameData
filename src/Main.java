b7403305fa562dda4f720aa8f9166acb53bc3361
CSteinmuller <chris.dungeonmaster@gmail.com>
2018-01-04 14:45:52 -0600

    ...

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    int j = 0;
+    int j = 2;
     int c = 1;
     //this plugin needs work //
 	}
